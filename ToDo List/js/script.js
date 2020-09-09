6ecc41a3afb3b31a46b94b7781144bc291db3d16
as-webagency <as.webagency2020@gmail.com>
2020-09-10 00:54:55 +0300

    add first to-do

--- ToDo List/js/script.js
+++ ToDo List/js/script.js
@@ -1,76 +1,86 @@
 'use strict';
 
-const todoControl = document.querySelector( '.todo-control' ),
-    headerInput = document.querySelector( '.header-input' ),
-    todoList = document.querySelector( '.todo-list' ),
-    todoCompleted = document.querySelector( '.todo-completed' ),
-    textTodo = document.querySelector( '.text-todo' );
+const todoControl = document.querySelector( '.todo-control' ), // Форма
+    headerInput = document.querySelector( '.header-input' ), // input который извлекает значение
+    todoList = document.querySelector( '.todo-list' ), // Добавляем дела
+    todoCompleted = document.querySelector( '.todo-completed' ); // Добавляем выполненые дела
 
-const todoData = [];
+// Массив в котором будут хранится дела (Из JS в Json формат)
+const todoData = JSON.parse( localStorage.getItem( 'savesToDo' ) ) || [];
 
+// Функция, которая рендерит(добавляет) дело
 const render = function () {
 
+    // Очищаем от предыдущих дел
     todoList.textContent = '';
     todoCompleted.textContent = '';
+    
+    // Перебираем массив
+    todoData.forEach( function ( item, index ) {
 
-    todoData.forEach( function ( item ) {
-
+        // Создаем список
         const li = document.createElement( 'li' );
         li.classList.add( 'todo-item' );
+
+        // Добавляем верстку
         li.innerHTML = '<span class="text-todo">' + item.value + '</span>' + 
             '<div class="todo-buttons">' + 
                 '<button class="todo-remove"></button>' + 
                 '<button class="todo-complete"></button>' + 
             '</div>';
 
+        // Добавляем элемент в начало списка
         if ( item.completed ) {
-            todoCompleted.append( li );
+            todoCompleted.prepend( li );
         } else {
-            todoList.append( li );
+            todoList.prepend( li );
         }
 
+        // Меняем статус дела
         const btnTodoComplete = li.querySelector( '.todo-complete' );
         btnTodoComplete.addEventListener( 'click', function () {
             item.completed = !item.completed;
+            saveToDo();
             render();
         });
 
+        // Удаляем блок li из списка
         const todoRemove = li.querySelector( '.todo-remove' );
         todoRemove.addEventListener( 'click', function () {
-            li.remove();
+            todoData.splice( index, 1 );
+            saveToDo();
+            render();
         });
-        
-    });
-    
-};  
 
+    });
+
+},
+
+// Функция конвертор - из JSON в JS
+saveToDo = () => {
+    localStorage.setItem( 'savesToDo', JSON.stringify( todoData ) );
+};
+
+// На форму навешиваем событие submit
 todoControl.addEventListener( 'submit', function ( event ) {
     event.preventDefault();
-
+    
+    // Новый объект для массива
     const newTodo = {
         value: headerInput.value,
         completed: false
     };
 
-    if ( headerInput.value !== '' ) {
-        todoData.push( newTodo );
-        headerInput.value = '';
-    }
+    // Проверка на пустую строку
+    if ( headerInput.value.trim() === '' ) return;
 
-    localStorage.textTodo = headerInput.value;
+    // Добавляем новый объект
+    todoData.push( newTodo );
+    // Очищаем input 
+    todoControl.reset();
 
+    saveToDo();
+    // Вызываем функцию рендера для обновления всех дел
     render();
 
 });
-
-// const showText = function () {
-//     headerInput.textContent = localStorage.headerInput;
-// };
-
-// todoControl.addEventListener( 'click', function () {
-
-//     localStorage.li = headerInput.value;
-//     showText();
-// });
-
-render();
